95e812ff4add3b39fce07716a32cd4834879a08e
Ramón Lozano <niksokupa@gmail.com>
2018-11-13 12:43:12 +0100

    productoservice update arreglado.

--- src/main/java/net/daw/service/ProductoService.java
+++ src/main/java/net/daw/service/ProductoService.java
@@ -111,7 +111,7 @@ public class ProductoService {
 
     public ReplyBean update() throws Exception {
         int iRes = 0;
-        ReplyBean oReplyBean = null;
+        ReplyBean oReplyBean;
         ConnectionInterface oConnectionPool = null;
         Connection oConnection;
         try {
@@ -123,8 +123,7 @@ public class ProductoService {
             oConnection = oConnectionPool.newConnection();
             ProductoDao oProductoDao = new ProductoDao(oConnection, ob);
             iRes = oProductoDao.update(oProductoBean);
-            oReplyBean.setStatus(200);
-            oReplyBean.setJson(Integer.toString(iRes));
+            oReplyBean = new ReplyBean(200, Integer.toString(iRes));
         } catch (Exception ex) {
             throw new Exception("ERROR: Service level: update method: " + ob + " object", ex);
         } finally {
